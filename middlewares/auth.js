097cd5c069a6390b8189a32619bff11070949a66
Alberto <albmartinmateos@gmail.com>
2025-06-09 22:38:42 +0200

    Añadido capa middleware de autorización

---
+++ middlewares/auth.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+
+export default function auth(req, res, next) {
+    const token = req.header("Authorization")
+    if (!token) {
+        return res.status(401).json({ message: "Access denied. No token provided." });
+    }
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified; // Attach user info to request object
+        next(); // Call the next middleware or route handler
+    } catch (error) {
+        return res.status(400).json({ message: "Invalid token." });
+    }
+}
